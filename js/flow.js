fdde8c675055869d2d1cd28addd8d245a7d95fbc
Andrea Prandini <andrea.prandini@quix.it>
2019-07-30 18:48:55 +0200

    Flow.html

--- js/flow.js
+++ js/flow.js
@@ -43,23 +43,35 @@ jsPlumb.ready(function () {
 
         nodes.forEach(drawNode);
 
+        edgeList.forEach(drawEdge);
+
         //alert(data);
     });
 
 
+    edgeList = [];
 
 
     drawNode = function (node) {
         //alert(node.title);
-
         createNode(node);
+    }
 
+    drawEdge = function (edge) {
+        //alert(edge.source);
+        //edge.anchors = ['Right', 'Left'];
+        edge.endpoint = "Rectangle";
+        edge.endpointStyle= {"fill:": "black"}
+        var common = {
+            anchors: ['Right', 'Left']
+        }
 
-
+        jsPlumb.connect(edge, common);
     }
 
 
 
+    // Crea un nuovo nodo
     createNode = function (node) {
         var newNode = $("#node").clone(true, true);
         newNode.find('.title').text(node.title);
@@ -67,12 +79,51 @@ jsPlumb.ready(function () {
         newNode.css('left', node.positionX + 'px');
         newNode.css('top', node.positionY + 'px');
         newNode.css('visibility', 'visible');
+
+        var elementList = newNode.find('.node-element-list');
+
+        if (node.elements) {
+            for (var e = 0; e < node.elements.length; e++) {
+
+                var element = createElement(node.elements[e])
+                if (element) {
+                    elementList.append(element);
+                }
+
+
+            }
+
+        }
+
+
+
+
         newNode.appendTo(instance.getContainer());
         instance.draggable(newNode);
         return (newNode);
     };
 
 
+    createElement = function (element) {
+        if (element.type == 'text') {
+            var newElement = $("#node-element-text").clone(true, true);
+            newElement.attr("id", "element-" + element.id);
+            newElement.html(element.title);
+            return newElement;
+        }
+        if (element.type == 'edge') {
+            var newElement = $("#node-element-edge").clone(true, true);
+            newElement.attr("id", "element-" + element.id);
+            newElement.html(element.title);
+
+            var edge = {};
+            edge.source = "element-" + element.id;
+            edge.target = "node-" + element.elementData;
+            edgeList.push(edge)
+
+            return newElement;
+        }
+    }
 
 
 });
